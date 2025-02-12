8bbf52a278fb296c0b4bc0602aa9ccb28c1a6466
sociocreatives <ayaraerick@gmail.com>
2025-02-12 14:56:59 +0300

    validation

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,9 +7,10 @@ import axios from "axios";
 import { useRouter } from "next/navigation"
 import { useState } from "react";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { zodResolver } from '@hookform/resolvers/zod'; 
+import { zodResolver } from '@hookform/resolvers/zod';
 import { createTvetSchema } from "../api/tvets/createTvetSchema";
 import { z } from "zod";
+import Spinner from "@/components/Spinner";
 // import { Terminal } from "lucide-react"
 
 type FormData = z.infer<typeof createTvetSchema>;
@@ -20,40 +21,43 @@ const Aboutpage = () => {
     resolver: zodResolver(createTvetSchema),
   });
   const [error, setError] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
 
   const onSubmit = async (data: FormData) => {
     try {
+      setIsSubmitting(true);
       const response = await axios.post("/api/tvets", data);
       if (response.status === 201) {
         router.push('/');
       }
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
+      setIsSubmitting(false);
       setError('An error occurred while creating the TVET');
     }
 
   };
 
   return (
-    <div className="flex flex-col items-start justify-start w-[1400px] mx-auto"> 
-    {error &&
-      <Alert variant="destructive" className="mb-4 bg-red-100">
-        <AlertTitle className="font-bold text-red-500">Alert!</AlertTitle>
-        <AlertDescription className="text-red-500">
-          {error}
-        </AlertDescription>
-      </Alert>}
-      <h1>About Page</h1>
+    <div className="flex flex-col items-start justify-start w-[1400px] mx-auto">
+      {error &&
+        <Alert variant="destructive" className="mb-4 bg-red-100">
+          <AlertTitle className="font-bold text-red-500">Alert!</AlertTitle>
+          <AlertDescription className="text-red-500">
+            {error}
+          </AlertDescription>
+        </Alert>}
+      <h1 className="my-4">About Page</h1>
       <form onSubmit={handleSubmit(onSubmit)} className="w-full">
         <Input
           {...register("title")} // Ensure the input is registered properly
-          className='mb-4 outline-none'
+          className=' outline-none'
           placeholder='Enter your name'
         />
-        {errors.title && <p className="text-red-500">Title is required</p>}
-        <Button variant="outline" className='w-full' type="submit">
-          Submit
+        {errors.title && <small className="text-red-500">Title is required</small>}
+        <Button variant="outline" className='w-full' type="submit" disabled={isSubmitting}>
+          Submit {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
