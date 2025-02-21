26c0ecb60ea6b8209458282194a5efb256508485
sociocreatives <ayaraerick@gmail.com>
2025-02-21 14:06:35 +0300

    editing the fist page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -27,7 +27,7 @@ const NavigationBar = () => {
     };
 
     return (
-        <header className="border-b border-gray-200 dark:border-gray-700 fixed top-0 left-0 right-0 z-10">
+        <header className="border-b border-gray-200 dark:border-gray-700  top-0 left-0 right-0 z-10">
         <nav className="bg-white dark:bg-gray-900 py-2">
             <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16 items-center">
