3ea7635ce3c59e51aa3d9823adf8149c007507ce
sociocreatives <ayaraerick@gmail.com>
2025-02-24 19:05:56 +0300

    categories

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import Image from 'next/image'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
 
 export default function HeroSection() {
   return (
-    <section className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 md:p-8 max-w-8xl mx-auto '>
-    <div className='bg-maingreen text-white p-4 md:p-8 rounded-md'></div>
-    <div className='bg-maingreen text-white p-4 md:p-8 rounded-md'></div>
+    <section className='container w-full mx-auto'>
+      <div className='grid gap-4 grid-cols-1 sm:grid-cols-12 mt-4 mb-4 '>
+        <div className='flex flex-col justify-center  col-span-8 md:w-full bg-maingreen text-white p-4 md:p-16 rounded-3xl h-full' >
+        <h1 className='text-4xl font-black text-mainyellow p-4'>Get the right local Pro</h1>
+        <h1 className='text-4xl font-black text-mainyellow'>in your area for any project.</h1>
+        <div className='flex gap-4 justify-center items-center mt-4 p-2 rounded-md bg-white'>
+        <Input type='text' className='w-full h-10 p-2  text-white bg-white rounded-md outline-none' placeholder='Enter your zip code' />
+        <Button className='bg-mainyellow text-maingreen'>Search</Button>
+        </div>
+        <p className=' text-white  mt-4 text-sm'>We have the best SKILLS and custom services for your PROJECT Go Premium</p>
+        </div>
+        <div className='text-white rounded-md col-span-4 md:block hidden'>
+          <Image 
+          src='/images/foursquare.png' 
+          alt='mrkim logo' 
+          layout='responsive'
+          width={2400} 
+          height={16}
+           />
+        </div>
+      </div>
     </section>
   )
 }
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,33 @@
 "use client"
-import Loader from '@/app/components/Loader';
+// import Loader from '@/app/components/Loader';
 // import SignOut from '@/app/components/Signout';
-import { useSession } from 'next-auth/react';
+// import { useSession } from 'next-auth/react';
 // import { useTranslations } from 'next-intl';
 import React from 'react'
+import HeroSection from './components/Hero'
 // import HeroSection from './components/Hero';
 
 export default function Home() {
   // const t = useTranslations('HomePage');
   // console.log(t('title'));
-  const { data: session, status } = useSession();
-  if (status === "loading") return <Loader/>;
+  // const { data: session, status } = useSession();
+  // if (status === "loading") return <Loader/>;
 
 
   return (
-    <div className='container my-4 mt-25'>
-     {session ? (
-        <p>Welcome, {session.user?.name}!</p>
-      ) : (
-        <p>You are not logged in.</p>
-      )}
-
-      {/* <HeroSection/>
-
-      <SignOut/> */}
+    <div className='flex items-center'>
+     <HeroSection/>
     </div>
   )
 }
 
 
+// {session ? (
+//   <p>Welcome, {session.user?.name}!</p>
+// ) : (
+//   <p>You are not logged in.</p>
+// )}
 
-// <Image
-// aria-hidden
-// src="/globe.svg"
-// alt="Globe icon"
-// width={16}
-// height={16}
-// />
+// {/* <HeroSection/>
 
-
-// import Image from "next/image";
+// <SignOut/> */}
